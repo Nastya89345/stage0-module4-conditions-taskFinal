3897dfb3a2c2594504fd5ae0d3d7ce075c6f6add
Nastya Belova <nastjabelova0@gmail.com>
2024-06-21 23:08:00 +0300

    done

--- src/main/java/school/mjc/stage0/conditions/finalTask/DaysInMonth.java
+++ src/main/java/school/mjc/stage0/conditions/finalTask/DaysInMonth.java
@@ -2,6 +2,31 @@ package school.mjc.stage0.conditions.finalTask;
 
 public class DaysInMonth {
     public void printDays(int year, int month) {
-
+        String result = switch (month){
+            case 4, 6, 9, 11 -> "30";
+            case 1, 3, 5, 7, 10, 12 -> "31";
+            case 2 ->{
+                if(isleap(year)){
+                    yield "29";
+                }else {
+                    yield "28";
+                }
+            }
+            default -> "invalid date";
+        };
+        System.out.println(result);
+    }
+    private static boolean isleap (int year){
+        boolean result;
+        if(year % 400 == 0){
+            result = true;
+        }else if(year % 100 == 0){
+            result = false;
+        }else if(year % 4 == 0){
+            result = true;
+        }else{
+            result = false;
+        }
+        return result;
     }
 }
--- src/main/java/school/mjc/stage0/conditions/finalTask/TriangleSidesValidator.java
+++ src/main/java/school/mjc/stage0/conditions/finalTask/TriangleSidesValidator.java
@@ -2,6 +2,19 @@ package school.mjc.stage0.conditions.finalTask;
 
 public class TriangleSidesValidator {
     public void validate(double firstSide, double secondSide, double thirdSide) {
-
+        double max;
+        if(firstSide > secondSide && firstSide > thirdSide){
+            max = firstSide;
+        }else if(firstSide < secondSide && secondSide > thirdSide){
+            max = secondSide;
+        }else {
+            max = thirdSide;
+        }
+        double sum = firstSide + secondSide + thirdSide;
+        if((sum - max) > max){
+            System.out.println("this is a valid triangle");
+        }else {
+            System.out.println("it's not a triangle");
+        }
     }
 }
